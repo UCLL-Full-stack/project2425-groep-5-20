8653b4bdeedbfc3c964557cef2560d07b7cab40c
Fnafke <elin.bachvarov@student.ucll.be>
2024-12-15 21:43:05 +0100

    added better title to the page

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Demo Project</title>
+        <title>FamList</title>
         <meta name="description" content="Exam app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
